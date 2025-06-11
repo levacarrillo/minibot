6fa91386ec5b864ec5c0bf63f584b5225535d996
Luis Nava <levacarrillo@gmail.com>
2025-06-11 02:25:45 -0600

    Clear code for GUI

--- gui/gui/app/controller.py
+++ gui/gui/app/controller.py
@@ -4,16 +4,13 @@ class Controller:
     def __init__(self, service = Service):
         self.service = service
 
-    def get_enviroments():
+    def get_enviroments(self):
         enviroments = ["Empty", "Home", "Stage"]
         return enviroments
 
-    def get_behavior_list():
+    def get_behavior_list(self):
         behavior_list = ["LIGHT_FOLLOWER", "SM_DESTINATION", "SM_AVOID_OBSTACLES"]
         return behavior_list
 
-    def get_current_step():
+    def get_current_step(self):
         return '0'
-    
-    def resize_canvas(width, height):
-        print('withxheight')
