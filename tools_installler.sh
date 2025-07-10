9d863114e75d0753306486761b3c8a2664a6a451
Luis E. González Nava <42481482+levacarrillo@users.noreply.github.com>
2025-07-10 02:06:33 -0600

    Create tools_installler.sh

---
+++ tools_installler.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# VERIFY IF USER IS ROOT
+if [ "$EUID" -ne 0 ]; then
+  echo "❌ PLEASE RUN THIS SCRIPT AS A ROOT (sudo ./installer.sh)"
+  exit 1
+fi
+
+echo "🔄 UPDATING PACKAGES..."
+apt update && apt upgrade -y
+
+# PACKAGE LIST TO INSTALL BY APT
+APT_PACKAGES=(
+  git
+  vim
+  tmux
+  htop
+  curl
+  xterm
+  python3
+  screenfetch
+)
+
+echo "🔧 INSTALLING PACKAGES WITH APT..."
+for pkg in "${APT_PACKAGES[@]}"; do
+  echo "📦 INSTALLING $pkg..."
+  apt install -y "$pkg"
+done
+
+# VERIFY IF STAP IS INSTALLED
+if ! command -v snap &> /dev/null; then
+  echo "⚠️ SNAP IS NOT INSTALLED. INSTALLING SNAP..."
+  apt install -y snapd
+fi
+
+# SNAP LIST TO INSTALL
+SNAP_PACKAGES=(
+  code --classic
+)
+
+echo "🔧 INSTALLING PACKAGES WITH SNAP..."
+for snap_pkg in "${SNAP_PACKAGES[@]}"; do
+  echo "📦 INSTALLING $snap_pkg..."
+  snap install $snap_pkg
+done
+
+echo "✅ INSTALLATION COMPLETED."
