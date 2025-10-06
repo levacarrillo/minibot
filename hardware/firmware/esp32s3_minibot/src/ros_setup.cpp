bd78a8e7888441d05db200c79a1448acdfcb1de9
Luis Gonzalez <levacarrillo@gmail.com>
2025-10-06 16:54:08 -0600

    Add subscriber to firmware

--- hardware/firmware/esp32s3_minibot/src/ros_setup.cpp
+++ hardware/firmware/esp32s3_minibot/src/ros_setup.cpp
@@ -14,6 +14,7 @@ rcl_subscription_t motors_sub;
 rcl_publisher_t sensors_pub;
 
 std_msgs__msg__Int32MultiArray sensors_msg;
+std_msgs__msg__Float32MultiArray vels_msg;
 
 int32_t sensors_data[8];
 
@@ -39,6 +40,10 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
     }
 }
 
+void subscription_callback(const void * msgin) {
+    const std_msgs__msg__Float32MultiArray * msg = (const std_msgs__msg__Float32MultiArray *) msgin;
+}
+
 void setup_ros() {
     set_microros_serial_transports(Serial);
     delay(100);
@@ -46,12 +51,14 @@ void setup_ros() {
     RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
     RCCHECK(rclc_node_init_default(&node, "microcontroller_node", "", &support));
 
+    RCCHECK(rclc_subscription_init_default(&motors_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray), "motors_speed"));
     RCCHECK(rclc_publisher_init_default(&sensors_pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32MultiArray), "sensors"));
 
     const unsigned int timer_timeout = 1;
     RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(timer_timeout), timer_callback));
-    RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+    RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
     RCCHECK(rclc_executor_add_timer(&executor, &timer));
+    RCCHECK(rclc_executor_add_subscription(&executor, &motors_sub, &vels_msg, &subscription_callback, ON_NEW_DATA));
 }
 
 void loop_ros() {
