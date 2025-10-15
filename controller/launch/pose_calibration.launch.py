80e6b80724edff06bd57fe2eab497e0eac2f4f28
Luis Gonzalez <levacarrillo@gmail.com>
2025-10-15 12:58:25 -0600

    add pose_calibration node

---
+++ controller/launch/pose_calibration.launch.py
@@ -0,0 +1,44 @@
+import os
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument 
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    use_sim = LaunchConfiguration('use_sim')
+
+    params_path = os.path.join(
+        get_package_share_directory('controller'),
+        'config',
+        'calibration_params.yaml'
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim',
+            default_value='false',
+            description='True to launch for simulation or false for real mode'
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('mobile_base'),
+                    'launch',
+                    'mobile_base.launch.py'
+                ])
+            ]),
+            launch_arguments={'use_sim': use_sim}.items()
+        ),
+        Node(
+            package='controller',
+            executable='pose_calibration',
+            name='pose_calibration',
+            output='screen',
+            parameters=[params_path]
+        ),
+    ])
--- controller/src/pose_calibration.cpp
+++ controller/src/pose_calibration.cpp
@@ -5,6 +5,7 @@
 #include "interfaces/srv/set_vel_params.hpp"
 #include "interfaces/srv/odom_set_point.hpp"
 #include "interfaces/action/go_to_pose.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "tf2/LinearMath/Quaternion.h"
@@ -19,34 +20,35 @@ using GetVelParams = interfaces::srv::GetVelParams;
 using SetVelParams = interfaces::srv::SetVelParams;
 using GoalHandleGoToPose = rclcpp_action::ServerGoalHandle<GoToPose>;
 
-class PoseCommander : public rclcpp::Node {
+class PoseCalibration: public rclcpp::Node {
 public:
-    PoseCommander() : Node("pose_commander") {
-        RCLCPP_INFO(this->get_logger(), "INTITIALITZING POSE_COMMANDER NODE...");
-
+    PoseCalibration() : Node("pose_calibration") {
+        RCLCPP_INFO(this->get_logger(), "INTITIALITZING POSE CALIBRATION NODE...");
         this->declare_parameter("linear_velocity", this->linear_velocity);
         this->declare_parameter("angular_velocity", this->angular_velocity);
         cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
 
+	pose_pub_= this->create_publisher<std_msgs::msg::Float32MultiArray>("pose_to_calibrate", 10);
+
 	odom_sub_= this->create_subscription<nav_msgs::msg::Odometry>(
-	    "odom", 10, std::bind(&PoseCommander::odomCallback, this, _1));
+	    "odom", 10, std::bind(&PoseCalibration::odomCallback, this, _1));
 
 	set_point_= this->create_client<interfaces::srv::OdomSetPoint>("odom_set_point");
 
         set_vel_service = this->create_service<SetVelParams>(
             "set_vel_params",
-            std::bind(&PoseCommander::handler_set_velocity, this, _1, _2)
+            std::bind(&PoseCalibration::handler_set_velocity, this, _1, _2)
         );
         get_vel_service = this->create_service<GetVelParams>(
             "get_vel_params",
-            std::bind(&PoseCommander::handler_get_velocity, this, _1, _2)
+            std::bind(&PoseCalibration::handler_get_velocity, this, _1, _2)
         );
         action_server_ = rclcpp_action::create_server<GoToPose>(
             this,
             "go_to_pose",
-            std::bind(&PoseCommander::handle_goal, this, _1, _2),
-            std::bind(&PoseCommander::handle_cancel, this, _1),
-            std::bind(&PoseCommander::handle_accepted, this, _1)
+            std::bind(&PoseCalibration::handle_goal, this, _1, _2),
+            std::bind(&PoseCalibration::handle_cancel, this, _1),
+            std::bind(&PoseCalibration::handle_accepted, this, _1)
         );
     }
 
@@ -60,6 +62,7 @@ private:
     rclcpp::Service<SetVelParams>::SharedPtr set_vel_service;
     rclcpp::Client<OdomSetPoint>::SharedPtr set_point_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr pose_pub_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     
     void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
@@ -93,18 +96,19 @@ private:
 
     rclcpp_action::GoalResponse handle_goal(
         const rclcpp_action::GoalUUID &/*uuid*/,
-        std::shared_ptr<const GoToPose::Goal> /* goal */) {
-        // RCLCPP_INFO(this->get_logger(), "GOAL RECEIVED: TWIST->%.2f rad, ADVANCE->%.2f m", goal->angle, goal->distance);
+        std::shared_ptr<const GoToPose::Goal> goal) {
+	RCLCPP_INFO(this->get_logger(), "\n \n  NEW GOAL RECEIVED \n_______________________\n");
+        RCLCPP_INFO(this->get_logger(), "GOAL RECEIVED: TWIST->%.2f rad, ADVANCE->%.2f m", goal->angle, goal->distance);
         return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
 
     void handle_accepted(const std::shared_ptr<GoalHandleGoToPose> goal_handle) {
         // RCLCPP_INFO(this->get_logger(), "GOAL ACCEPTED");
-        std::thread{std::bind(&PoseCommander::execute, this, _1), goal_handle}.detach();
+        std::thread{std::bind(&PoseCalibration::execute, this, _1), goal_handle}.detach();
     }
 
     rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleGoToPose> /*goal_handle*/) {
-        RCLCPP_INFO(this->get_logger(), "CANCEL REQUESTED");
+        RCLCPP_WARN(this->get_logger(), "CANCEL REQUESTED");
         return rclcpp_action::CancelResponse::ACCEPT;
     }
 
@@ -121,18 +125,27 @@ private:
         const auto goal = goal_handle->get_goal();
         geometry_msgs::msg::Twist cmd;
 
+	auto pose_message = std_msgs::msg::Float32MultiArray();
+	pose_message.data.resize(4);
+	pose_message.data[0] = goal->distance;
+	pose_message.data[1] = goal->angle;
+	pose_message.data[2] = curr_distance;
+	pose_message.data[3] = curr_angle;
 
         feedback->feedback = "Twisting...";
         goal_handle->publish_feedback(feedback);
 
         cmd.angular.z = (goal->angle > 0 ? this->angular_velocity : - this->angular_velocity);
+	
+        RCLCPP_INFO(this->get_logger(), "\n MOVING ANGULAR \n");
 
 	while (abs(curr_angle) < abs(goal->angle)) {
-	    // RCLCPP_INFO(this->get_logger(), "CURR DISP->%f\tCURR ANGLE->%f", curr_distance, curr_angle); 
+	    RCLCPP_INFO(this->get_logger(), "CURR DISP->%f\tCURR ANGLE->%f", curr_distance, curr_angle); 
             cmd_vel_pub_->publish(cmd);
+       	    pose_pub_->publish(pose_message);
 
             if (goal_handle->is_canceling()) {
-                RCLCPP_INFO(this->get_logger(), "CANCELING MOVEMENT...");
+                RCLCPP_WARN(this->get_logger(), "CANCELING MOVEMENT...");
                 cmd.angular.z = 0.0;
                 cmd_vel_pub_->publish(cmd);
                 result->success = false;
@@ -146,9 +159,13 @@ private:
         goal_handle->publish_feedback(feedback);
         cmd.angular.z = 0.0;
         cmd.linear.x = (goal->distance > 0 ? this->linear_velocity : - this->linear_velocity);
+	
+        RCLCPP_INFO(this->get_logger(), "\n MOVING LINEAR \n");
 
         while (abs(curr_distance) < abs(goal->distance)) {
+	    RCLCPP_INFO(this->get_logger(), "CURR DISP->%f\tCURR ANGLE->%f", curr_distance, curr_angle); 
             cmd_vel_pub_->publish(cmd);
+            pose_pub_->publish(pose_message);
 
             if (goal_handle->is_canceling()) {
                 cmd.linear.x = 0.0;
@@ -174,6 +191,7 @@ private:
 	using ResponseFuture = rclcpp::Client<interfaces::srv::OdomSetPoint>::SharedFuture;
 	auto response_callback = [this](ResponseFuture future) {
 	  auto res = future.get();
+	  // RCLCPP_INFO(this->get_logger(), "RES-> %d", res.get()->done);
 	};
 
 	set_point_->async_send_request(request, response_callback);
@@ -186,7 +204,7 @@ private:
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
 
-    auto node = std::make_shared<PoseCommander>();
+    auto node = std::make_shared<PoseCalibration>();
     rclcpp::spin(node);
 
     rclcpp::shutdown();
