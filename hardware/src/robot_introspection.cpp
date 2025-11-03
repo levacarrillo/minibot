78dd5864709e0880ee17f90da2d9b1f5ae546efa
Luis Nava <levacarrillo@gmail.com>
2025-11-03 14:02:09 -0600

    Update robot_introspection for simul

--- hardware/src/robot_introspection.cpp
+++ hardware/src/robot_introspection.cpp
@@ -11,54 +11,62 @@ public:
         RCLCPP_INFO(this->get_logger(), "INITIALIZING ROBOT_INTROSPECTION NODE...");
         this->declare_parameter<int>("robot_id", this->robot_id);
         this->declare_parameter<std::string>("robot_name", this->robot_name);
-        this->declare_parameter<int>("temperature", this->temperature);
-        this->declare_parameter<double>("battery_voltage", this->battery_voltage);
-        this->declare_parameter<double>("battery_capacity", this->battery_capacity);
-        this->declare_parameter<int>("battery_cells_number", this->battery_cells_number);
-        this->declare_parameter<int>("battery_charge_percentage", this->battery_charge_percentage);
+        this->declare_parameter<float>("microcontroller_temperature", this->microcontroller_temperature);
+        this->declare_parameter<float>("raspberrypi_temperature", this->raspberrypi_temperature);
+        this->declare_parameter<std::vector<float>>("battery_voltage", this->battery_voltage);
+        this->declare_parameter<std::vector<int32_t>>("battery_charge_percentage", this->battery_charge_percentage);
         this->declare_parameter<std::string>("battery_supply_status", this->battery_supply_status);        
         
         publisher_ = this->create_publisher<interfaces::msg::RobotStatus>("robot_status", 10);
         
         timer_ = this->create_wall_timer(
-            500ms, std::bind(&RobotIntrospection::timer_callback, this)
+            100ms, std::bind(&RobotIntrospection::timer_callback, this)
         );
     }
+
     private:
+    int robot_id = 0;
+    std::string robot_name = "NO SELECTED";
+    float microcontroller_temperature = 0.0;
+    float raspberrypi_temperature = 0.0;
+    std::vector<float> battery_voltage = {0.0, 0.0};
+    std::vector<int32_t> battery_charge_percentage = {0, 0};
+    std::string battery_supply_status = "NO DEFINED";
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<interfaces::msg::RobotStatus>::SharedPtr publisher_;
+
     void timer_callback() {
         this->robot_id    = this->get_parameter("robot_id").as_int();
         this->robot_name  = this->get_parameter("robot_name").as_string();
-        this->temperature = this->get_parameter("temperature").as_int();
-        this->battery_voltage  = this->get_parameter("battery_voltage").as_double();
-        this->battery_capacity = this->get_parameter("battery_capacity").as_double();
-        this->battery_cells_number = this->get_parameter("battery_cells_number").as_int();
-        this->battery_charge_percentage = this->get_parameter("battery_charge_percentage").as_int();
+        this->microcontroller_temperature = this->get_parameter("microcontroller_temperature").as_double();
+        this->raspberrypi_temperature = this->get_parameter("raspberrypi_temperature").as_double();
+        auto battery_voltage = this->get_parameter("battery_voltage").as_double_array();
+        auto battery_charge_percentage  = this->get_parameter("battery_charge_percentage").as_integer_array();
         this->battery_supply_status = this->get_parameter("battery_supply_status").as_string();
+
+        this->battery_voltage.clear();
+        this->battery_charge_percentage.clear();
+        this->battery_voltage.reserve(battery_voltage.size());
+        this->battery_charge_percentage.reserve(battery_charge_percentage.size());
+
+        for (auto &v : battery_voltage) {
+            this->battery_voltage.push_back(static_cast<double>(v));
+        }
+        for (auto &v : battery_charge_percentage) {
+            this->battery_charge_percentage.push_back(static_cast<int32_t>(v));
+        }
 
         auto message = interfaces::msg::RobotStatus();
         message.robot_id = this->robot_id;
         message.robot_name = this->robot_name;
-        //message.temperature = this->temperature;
-        // message.battery_voltage = this->battery_voltage;
-        // message.battery_capacity = this->battery_capacity;
-        // message.battery_cells_number = this->battery_cells_number;
-        // message.battery_charge_percentage = this->battery_charge_percentage;
+        message.microcontroller_temperature = this->microcontroller_temperature;
+        message.raspberrypi_temperature = this->raspberrypi_temperature;
+        message.battery_voltage = this->battery_voltage;
+        message.battery_charge_percentage = this->battery_charge_percentage;
         message.battery_supply_status = this->battery_supply_status;
 
         publisher_->publish(message);
     }
-
-    int robot_id;
-    std::string robot_name;
-    int temperature;
-    double battery_voltage;
-    double battery_capacity;
-    int battery_cells_number;
-    int battery_charge_percentage;
-
-    std::string battery_supply_status;
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<interfaces::msg::RobotStatus>::SharedPtr publisher_;
 };
 
 int main(int argc, char * argv[]) {
