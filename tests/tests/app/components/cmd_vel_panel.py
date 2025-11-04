7824e8d3992f494e17bccbc6d9a9813838b0b9ed
Luis Nava <levacarrillo@gmail.com>
2025-11-03 18:43:31 -0600

    Update velocity params for test GUI

--- tests/tests/app/components/cmd_vel_panel.py
+++ tests/tests/app/components/cmd_vel_panel.py
@@ -4,8 +4,8 @@ from tkinter import *
 class CmdVelPanel:
     def __init__(self, context):
 
-        self.linear_vel  = StringVar(value = '0.20m/s')
-        self.angular_vel = StringVar(value = '0.30rad/s')
+        self.linear_vel  = StringVar(value = '0.30m/s')
+        self.angular_vel = StringVar(value = '3.14rad/s')
 
         frame = LabelFrame(context.content, text = 'Move robot')
 
@@ -16,18 +16,18 @@ class CmdVelPanel:
         buton_arrow_stop  = Button( frame, text = '||', width = 2, command = lambda: context.move_robot('STOP'))
         buton_arrow_up    = Button( frame, text = '^',  width = 2, command = lambda: context.move_robot('FORWARD'))
         buton_arrow_down  = Button( frame, text = 'v',  width = 2, command = lambda: context.move_robot('BACKWARD'))
-        linear_vel_entry  = Spinbox(frame, from_ = 0.0, to = 2.0, increment = 0.01, textvariable = self.linear_vel,  width = 8)
-        angular_vel_entry = Spinbox(frame, from_ = 0.0, to = 2.0, increment = 0.01, textvariable = self.angular_vel, width = 8)
+        linear_vel_entry  = Spinbox(frame, from_ = 0.0, to = 1.0, increment = 0.01, textvariable = self.linear_vel,  width = 8)
+        angular_vel_entry = Spinbox(frame, from_ = 0.0, to = 9.0, increment = 0.01, textvariable = self.angular_vel, width = 8)
 
         frame             .grid(column = 3, row = 1, sticky = (N, W), padx = (5, 5),  pady = (5, 5),  columnspan = 3)
         buton_arrow_left  .grid(column = 0, row = 1, sticky = (N, W), padx = (5, 0),  pady = (0, 0),  columnspan = 2)
         buton_arrow_right .grid(column = 4, row = 1, sticky = (N, W), padx = (5, 10), pady = (0, 0),  columnspan = 2)
-        buton_arrow_stop  .grid(column = 2, row = 1, sticky = (N, W), padx = (5, 0), pady = (0, 0),   columnspan = 2)
-        buton_arrow_up    .grid(column = 2, row = 0, sticky = (N, W), padx = (5, 0), pady = (10, 5),  columnspan = 2)
-        buton_arrow_down  .grid(column = 2, row = 2, sticky = (N, W), padx = (5, 0), pady = (5, 10),  columnspan = 2)
-        label_linear_vel  .grid(column = 0, row = 3, sticky = (N, W), padx = (5, 0), pady = (5, 10),  columnspan = 3)
+        buton_arrow_stop  .grid(column = 2, row = 1, sticky = (N, W), padx = (5, 0),  pady = (0, 0),  columnspan = 2)
+        buton_arrow_up    .grid(column = 2, row = 0, sticky = (N, W), padx = (5, 0),  pady = (10, 5), columnspan = 2)
+        buton_arrow_down  .grid(column = 2, row = 2, sticky = (N, W), padx = (5, 0),  pady = (5, 10), columnspan = 2)
+        label_linear_vel  .grid(column = 0, row = 3, sticky = (N, W), padx = (5, 0),  pady = (5, 10), columnspan = 3)
         linear_vel_entry  .grid(column = 3, row = 3, sticky = (N, W), padx = (5, 10), pady = (5, 10), columnspan = 3)
-        label_angular_vel .grid(column = 0, row = 4, sticky = (N, W), padx = (5, 0), pady = (5, 10),  columnspan = 3)
+        label_angular_vel .grid(column = 0, row = 4, sticky = (N, W), padx = (5, 0),  pady = (5, 10), columnspan = 3)
         angular_vel_entry .grid(column = 3, row = 4, sticky = (N, W), padx = (5, 10), pady = (5, 10), columnspan = 3)
 
         context.set_cmd_vel_panel(self)
